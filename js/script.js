4f723ca5a5fac5f760cea2d1492ba75ff8db2659
Simonhamel1 <156895661+Simonhamel1@users.noreply.github.com>
2025-02-03 19:51:56 +0100

    commit debut

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Le site de l\'agence de voyage est prêt.');
+
+    // Afficher un pop-up après 3 secondes
+    setTimeout(function() {
+        alert('Profitez de nos offres spéciales sur les voyages!');
+    }, 3000);
+});

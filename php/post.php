b0119d8dae70bfe5bf9cd766b822ee01431119cf
Clab-ewan <clabewan@gmail.com>
2025-03-17 13:18:24 +0100

    finishing post method

--- php/post.php
+++ php/post.php
@@ -1,13 +1,25 @@
 <?php
     $_POST['Password'] = hash('sha256', $_POST['Password']);
-    $Id = hash('sha256', hash('sha256', $_POST['Email']) . $_POST['Password']); // a746a7c5d2a5eee0dded7a4876bb2e7f9468f678c2d43313bdd89371731ca5bb
+    $Id = hash('sha256', hash('sha256', $_POST['Email']) . $_POST['Password']); 
     $DataArray = $_POST; 
     $DataArray["Id"] = $Id;
     $DataArray = json_encode($DataArray);
-    echo $DataArray;
-    // isn't able to write after the post method
-    $absolute_path = __DIR__ . "/donnees/DB.json";
-    $fichier = fopen($absolute_path, "a+");
-    fwrite($fichier, $DataArray);
-    fclose($fichier);
+    $absolute_path = "../donnees/DB.json";
+    
+    // Lire le contenu actuel du fichier
+    $content = file_get_contents($absolute_path);
+    
+    // Si le fichier est vide, initialiser avec un tableau JSON
+    if (empty($content)) {
+        $content = "[]";
+    }
+    
+    // Convertir le contenu en tableau PHP
+    $jsonArray = json_decode($content, true);
+    
+    // Ajouter les nouvelles données
+    $jsonArray[] = json_decode($DataArray, true);
+    
+    // Reconvertir en JSON et écrire dans le fichier
+    file_put_contents($absolute_path, json_encode($jsonArray, JSON_PRETTY_PRINT));
 ?>

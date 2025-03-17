7c5e761ae422aa43eb02fc78ba8e1b8deb063ea8
Simonhamel1 <156895661+Simonhamel1@users.noreply.github.com>
2025-03-17 14:06:28 +0100

    marine

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -7,7 +7,7 @@ const fileCancelButton = fileUploadWrapper.querySelector("#file-cancel");
 const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 // API setup
-const API_KEY = process.env.API_KEY;
+const API_KEY = "AIzaSyDfxwjgKGCEnfUHkQGBq7F6br650MnF-Hg";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 // Initialize user message and file data
 const userData = {
--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Le site de l\'agence de voyage est prêt.');
-
-    // Afficher un pop-up après 3 secondes
-    setTimeout(function() {
-        alert('Profitez de nos offres spéciales sur les voyages!');
-    }, 3000);
-});

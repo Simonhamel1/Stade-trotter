0401cf90aeb57d400a8b99b68b769d115fcdf885
Clab-ewan <clabewan@gmail.com>
2025-05-01 15:04:39 +0200

    avancée js modification_profil

--- js/modification_utilisateurs.js
+++ js/modification_utilisateurs.js
@@ -4,30 +4,27 @@ function modification_utilisateurs(buttonId) {
     const dernierChar = buttonId.charAt(buttonId.length - 1);
     const input_id = "inputbutton" + dernierChar;
     document.getElementById(input_id).readOnly = false;
-    
     // Valeur initale de l'input
-    const valeur_initale = document.getElementById(input_id).value;
-
+    const valeur_initale = document.getElementById(input_id).value;     
     // Mise en place des nouveaux boutons
     const valider_prefixe = "valider";
     const annuler_prefixe = "annuler";
     document.getElementById(buttonId).innerHTML = '<button class="enregistrer_modifications" id="' + valider_prefixe +'_'+ buttonId + '" onclick="enregistrer_changements(this.id);" value="enregistrer"> Enregistrer </button> <button class="annuler_modifications" id="' + annuler_prefixe +'_'+ buttonId + '" onclick="annuler_changements(this.id);" value="annuler"> Annuler </button>';
-    
 }
 
 function enregistrer_changements(id) {
     // For module4 Change the value in session and in files and display the new right value (session is enough)
-    // valider la modification
-    // modifier attribut read-only
     const dernierChar = id.charAt(id.length - 1);
-    const valider_id = "inputbutton" + dernierChar;
-    // accéder à l'input via son Id
-}
+    const input_id = "inputbutton" + dernierChar;
+    document.getElementById(input_id).readOnly = true;
+    const buttonId = "button" + dernierChar;
+    document.getElementById(buttonId).innerHTML = '<button class="permettre_modifications" onclick="modification_utilisateurs(\'' + buttonId + '\');" value="modifier"> Modifier </button>'
+} 
 
 
 function annuler_changements(id) {
     // Put it back to normal with the old data
     const dernierChar = id.charAt(id.length - 1);
-    const annuler_id = "inputbutton" + dernierChar;
-    // accéder à l'input via son Id
+    const input_id = "inputbutton" + dernierChar;
+    document.getElementById(input_id).readOnly = true;
 }
